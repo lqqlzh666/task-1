8d54241a5be8751cc3f08c00fc35e1cdeaf4c409
lqqlzh666 <2095743987@qq.com>
2024-03-26 19:47:09 +0800

    “task2”

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
+import './api/mock'
 
 Vue.config.productionTip = false
 
